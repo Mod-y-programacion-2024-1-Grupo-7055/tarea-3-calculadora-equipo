9807af6b4f85005a2d63a374b6abf185ba6a8bf4
edvar21 <eduardovargas21@ciencias.unam.mx>
2023-11-19 02:34:00 -0600

    interfaz grafica implementada

---
+++ src/EjecutarCalculadora.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * Clase que se encarga de ejecutar el programa
+ */ 
+
+import javax.swing.*;
+
+public class EjecutarCalculadora {
+    public static void main(String[] args)
+    {
+            new Calculadora();
+    }
+}
